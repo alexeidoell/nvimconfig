3498f33468184dc5f5782de9aadfb13bff97b1c4
Alexei Doell <alexeidoell@gmail.com>
2024-11-19 15:54:00 -0600

    new theme

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -1,4 +1,37 @@
 return {
+{
+        "nvim-treesitter/nvim-treesitter",
+        lazy = true,
+        event = "BufRead",
+        opts = {
+            -- A list of parser names, or "all"
+            ensure_installed = { "vimdoc", "c", "lua", "cpp", "java" },
+
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            highlight = {
+                -- `false` will disable the whole extension
+                enable = true,
+
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = false,
+            },
+        }
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        lazy = true,
+        opts = {},
+    },
+
 	{
 		"VonHeikemen/lsp-zero.nvim",
 		branch = "v3.x",
@@ -24,6 +57,7 @@ return {
 		  {'rafamadriz/friendly-snippets'},
 		},
         opts = function()
+            require("nvim-treesitter").setup()
             local lsp_zero = require('lsp-zero')
 
             lsp_zero.on_attach(function(client, bufnr)
@@ -85,38 +119,5 @@ return {
                 }),
             })
         end,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        lazy = true,
-        event = "BufRead",
-        opts = {},
-        opts = {
-            -- A list of parser names, or "all"
-            ensure_installed = { "vimdoc", "c", "lua", "cpp", "java" },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
-
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
-                additional_vim_regex_highlighting = false,
-            },
-        }
-    },
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        lazy = true,
-        opts = {},
     },
 }
