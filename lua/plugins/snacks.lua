b96bbd6f0c9d64ff04f8e5c04a25ed348dcc0f33
alexeidoell <alexeidoell@gmail.com>
2024-12-01 18:45:16 -0600

    removed header, gotta figure out how to change startup stats

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,13 +7,12 @@ return {
                 width = 18,
                 preset = {
                     keys = {
-                        { icon = "", key = "e", desc = "explorer", action = "<CMD>Oil<CR>", },
+                        { icon = "", key = "e", desc = "explorer", action = "<CMD>Oil<CR>" },
+                        { icon = "", key = "r", desc = "recent file", section = "session" },
                         { icon = "", key = "f", desc = "find file", action = ":lua Snacks.dashboard.pick('files')", },
                         { icon = "", key = "n", desc = "new file", action = ":ene | startinsert" },
-                        { icon = "", key = "g", desc = "grep text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-                        { icon = "", key = "r", desc = "recent file", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                        { icon = "", key = "s", desc = "search text", action = ":lua Snacks.dashboard.pick('live_grep')" },
                         { icon = "", key = "c", desc = "config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})", },
-                        { icon = "", key = "s", desc = "session", section = "session" },
                         { icon = "", key = "l", desc= "lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
                         { icon = "", key = "q", desc = "quit", action = function()
                             require("confirm-quit").confirm_quit()
@@ -24,6 +23,10 @@ return {
                 formats = {
                     key = { "" },
                 },
+                sections = {
+                    { section = "keys", gap = 1, padding = 1 },
+                    { section = "startup"},
+                }
             },
             notifier = {
                 style = "compact",
