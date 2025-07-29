fef572a7f9755335a5591e34e52f29eabbbf30a6
Alexei Doell <alexei.doell@calian.com>
2025-07-29 15:51:14 -0600

    change file search keybinds

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -62,9 +62,9 @@ return {
             scratch = { enabled = true },
         },
         keys = {
-            {"<leader>pf", function() Snacks.picker.files() end },
+            {"<leader>pd", function() Snacks.picker.files() end },
             {"<leader>ps", function() Snacks.picker.grep() end },
-            {"<leader>pr", function() Snacks.picker.smart() end },
+            {"<leader>pf", function() Snacks.picker.smart() end },
             {"<leader>pg", function() Snacks.picker.git_files() end },
             { "<leader>.",  function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
             { "<leader>S",  function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
