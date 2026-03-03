2281d5154d36f0473a37289f569c20c44fba3eaa
Alexei Doell <alexeidoell@gmail.com>
2026-03-03 09:21:38 -0600

    nes support

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,6 +4,20 @@ return {
         opts = function()
             vim.g.copilot_nes_debounce = 500
             vim.lsp.enable("copilot_ls")
+            vim.keymap.set("n", "<C-l>", function()
+                local bufnr = vim.api.nvim_get_current_buf()
+                local state = vim.b[bufnr].nes_state
+                if state then
+                    local _ = require("copilot-lsp.nes").walk_cursor_start_edit()
+                    or (
+                        require("copilot-lsp.nes").apply_pending_nes()
+                        and require("copilot-lsp.nes").walk_cursor_end_edit()
+                    )
+                    return nil
+                else
+                    return nil
+                end
+            end, { desc = "Accept Copilot NES suggestion", expr = true })
         end,
     },
 }
